42fbc5b107a938dfd1b8629a4e52da96cf384252
tosinadedapo <tosinoreka@yahoo.com>
2024-03-20 18:16:14 +0100

    Sent email completed

--- Home.py
+++ Home.py
@@ -18,19 +18,19 @@ df = pandas.read_csv("data.csv")
 
 with col1:
     for index, row in df[:4].iterrows():
+        st.image("images/" + row["image"])
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
-        st.image("images/"+row["image"])
-
 
 with col2:
     for index, row in df[4:8].iterrows():
+        st.image("images/" + row["image"])
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
-        st.image("images/" + row["image"])
 
 with col3:
     for index, row in df[8:].iterrows():
+        st.image("images/" + row["image"])
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
-        st.image("images/" + row["image"])
+
--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email  address")
-    message = st.text_area("Your message here")
+    subject = st.text_input("Subject")
+    raw_message = st.text_area("Your message here")
+    message = f"{raw_message}"
     button = st.form_submit_button("Submit")
     if button:
-        message = message + user_email
-
+        send_email(message, user_email)
+        st.info("Your email was sent successfully")
